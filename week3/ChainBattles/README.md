d556e28f84700a4733850f87684bd27230a1aca5
Peter Fan <peter.j.fan@gmail.com>
2022-06-11 11:17:21 -0400

    Updated week3 with challenges

--- week3/ChainBattles/README.md
+++ week3/ChainBattles/README.md
@@ -3,13 +3,20 @@ Deployed week 3 contract on Polygon Mumbai testnet on 5/31/2022.
 
 Contract address: 0xbC3DcF1f3Edfc7Ee80d78EcA2982C5Be84B31450
 
-Contract address (with Challenges Implemented): 
+Contract address (with Challenges Implemented): 0x5E3a9F25ac4912c33a638158D9AEF0cd08c53698
 
 # Setup
 ```
+yarn
+yarn add hardhat
+yarn add @openzeppelin/contracts
 ```
+
+Fill in .env file with appropriate values.
 
 # Deploy and Verify
 ```
+npx hardhat compile
+npx hardhat run scripts/deploy.js --network mumbai
+npx hardhat verify --network mumbai YOUR_SMARTCONTRACT_ADDRESS
 ```
-
--- week3/ChainBattles/contracts/ChainBattles.sol
+++ week3/ChainBattles/contracts/ChainBattles.sol
@@ -11,8 +11,15 @@ contract ChainBattles is ERC721URIStorage {
     using Strings for uint256;
     using Counters for Counters.Counter;
 
+    struct characterStats { 
+        uint256 levels;
+        uint256 speed;
+        uint256 strength;
+        uint256 life;
+    }
+
     Counters.Counter private _tokenIds;
-    mapping(uint256 => uint256) public tokenIdToLevels;
+    mapping(uint256 => characterStats) public tokenIdToCharStats;
 
     constructor() ERC721 ("Chain Battles", "CBTLS"){
         
@@ -35,8 +42,23 @@ contract ChainBattles is ERC721URIStorage {
     }
 
     function getLevels(uint256 tokenId) public view returns (string memory) {
-        uint256 levels = tokenIdToLevels[tokenId];
+        uint256 levels = tokenIdToCharStats[tokenId].levels;
         return levels.toString();
+    }
+
+    function getSpeed(uint256 tokenId) public view returns (string memory) {
+        uint256 speed = tokenIdToCharStats[tokenId].speed;
+        return speed.toString();
+    }
+
+    function getStrength(uint256 tokenId) public view returns (string memory) {
+        uint256 strength = tokenIdToCharStats[tokenId].strength;
+        return strength.toString();
+    }
+
+    function getLife(uint256 tokenId) public view returns (string memory) {
+        uint256 life = tokenIdToCharStats[tokenId].life;
+        return life.toString();
     }
 
     function generateCharacter(uint256 tokenId) public returns(string memory){
@@ -46,6 +68,9 @@ contract ChainBattles is ERC721URIStorage {
             '<rect width="100%" height="100%" fill="black" />',
             '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',"Warrior",'</text>',
             '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">', "Levels: ",getLevels(tokenId),'</text>',
+            '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">', "Speed: ",getSpeed(tokenId),'</text>',
+            '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">', "Strength: ",getStrength(tokenId),'</text>',
+            '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">', "Life: ",getLife(tokenId),'</text>',
             '</svg>'
         );
         return string(
@@ -56,19 +81,36 @@ contract ChainBattles is ERC721URIStorage {
         );
     }
 
+    function generatePseudoRandomNumTimestamp() internal view returns(uint256){  
+        return uint256(keccak256(abi.encodePacked(block.timestamp)));
+    }
+
+    function generatePseudoRandomNumBlockhash() internal view returns(uint256){
+        return uint256(blockhash(block.number-1));
+    }
+
+    function generatePseudoRandomNumIncrementing(uint256 number) internal pure returns(uint256){
+        return uint256(keccak256(abi.encodePacked(number)));
+    }
+
     function mint() public {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _safeMint(msg.sender, newItemId);
-        tokenIdToLevels[newItemId] = 0;
+        tokenIdToCharStats[newItemId] = characterStats(
+            0, 
+            generatePseudoRandomNumTimestamp() % 10, // speed
+            generatePseudoRandomNumBlockhash() % 10, // strength
+            generatePseudoRandomNumIncrementing(newItemId) % 10  // life
+        );
         _setTokenURI(newItemId, getTokenURI(newItemId));
     }
 
     function train(uint256 tokenId) public {
         require(_exists(tokenId));
         require(ownerOf(tokenId) == msg.sender, "You must own this NFT to train it!");
-        uint256 currentLevel = tokenIdToLevels[tokenId];
-        tokenIdToLevels[tokenId] = currentLevel + 1;
+        uint256 currentLevel = tokenIdToCharStats[tokenId].levels;
+        tokenIdToCharStats[tokenId].levels = currentLevel + 1;
         _setTokenURI(tokenId, getTokenURI(tokenId));
     }
     
