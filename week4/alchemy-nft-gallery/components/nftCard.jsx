6753328f09a381592e2be740df6f12856cbae2c9
Peter Fan <peter.j.fan@gmail.com>
2022-08-08 18:06:41 -0400

    Added copy icons (challenge #1)

--- week4/alchemy-nft-gallery/components/nftCard.jsx
+++ week4/alchemy-nft-gallery/components/nftCard.jsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCopy } from '@fortawesome/free-solid-svg-icons'
+
 export const NFTCard = ({ nft }) => {
 
     return (
@@ -8,11 +11,14 @@ export const NFTCard = ({ nft }) => {
             <div className="flex flex-col y-gap-2 px-2 py-3 bg-slate-100 rounded-b-md h-110 ">
                 <div className="">
                     <h2 className="text-xl text-gray-800">{nft.title}</h2>
-                    <p className="text-gray-600">Id: {nft.id.tokenId}</p>
-                    <p className="text-gray-600" >{`${nft.contract.address}`}</p>
+                    <p className="text-gray-600">Id: {nft.id.tokenId.substr(nft.id.tokenId.length - 4)}</p>
+                    <p className="text-gray-600"><FontAwesomeIcon icon={faCopy} onClick={() => {navigator.clipboard.writeText(nft.contract.address); }} />{` ${nft.contract.address.substr(0, 4)}...${nft.contract.address.substr(nft.contract.address.length - 4)}`}</p>
                 </div>
                 <div className="flex-grow mt-2">
-                    <p className="text-gray-600">{nft.description}</p>
+                    <p className="text-gray-600">{nft.description?.substr(0, 150)}</p>
+                </div>
+                <div className="flex justify-center mb-1">
+                    <a target={"_blank"} href={`https://etherscan.io/token/${nft.contract.address}`} className="py-2 px-4 bg-blue-500 w-1/2 text-center rounded-m text-white cursor-pointer">View on etherscan</a>
                 </div>
             </div>
         </div>
